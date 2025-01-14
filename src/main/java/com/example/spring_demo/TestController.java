2afa5c72c92fa9f56787d3058da345a915a011c0
parvez76 <parvez3676@gmail.com>
2025-01-14 12:44:54 +0600

    initial commit

---
+++ src/main/java/com/example/spring_demo/TestController.java
@@ -0,0 +1,21 @@
+package com.example.spring_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    @GetMapping("/data")
+    public String getData() {
+       return "Hello AWS!";
+    }
+
+
+    // @GetMapping("/message")
+    // public String getMessage() {
+    //    return "Hello AWS 2!";
+    // }
+}
